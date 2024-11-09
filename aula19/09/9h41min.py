d488060f00a301f812e37c6e549e21f7a72142b9
CainanEduardo <cainan.bento10@gmail.com>
2024-11-09 13:25:58 +0000

    salvar

--- aula19/09/9h41min.py
+++ aula19/09/9h41min.py
@@ -2,17 +2,17 @@ import os
 
 os.system ("cls || clear")
 
-# Leitura do número de maçãs compradas
-numero_macas = int(input("Digite o número de maçãs compradas: "))
+# Pedir para digitar o número de maçãs
+numero_maçãs = int(input("Digite o número de maçãs compradas: "))
 
-# Definição do preço unitário
-if numero_macas < 12:
-    preco_unitario = 1.30  # preço de cada maçã se forem menos de 12
+# Definição do preço de cada.
+if numero_maçãs < 12:
+    preço_unitario = 1.30  # preço de cada maçã se forem menos de 12
 else:
-    preco_unitario = 1.00  # preço de cada maçã se forem 12 ou mais
+    preço_unitario = 1.00  # Preço de cada maçã se forem 12 ou mais
 
-# Cálculo do custo total
-custo_total = numero_macas * preco_unitario
+# Cálculo do custo total.
+custo_total = numero_maçãs * preço_unitario
 
-# Exibição do resultado
-print(f"O custo total da compra é R$ {custo_total:.2f}")
+# Exibição do resultado.
+print(f"O custo da compra é R$ {custo_total: }")
